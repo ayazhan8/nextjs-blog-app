8546cda72baa2f0e50ef7cccf43db6d49bed3f03
ayazhan8 <ayerikkyzy@gmail.com>
2024-08-19 23:28:29 +0100

    add path for Sanity

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'ipc0h1oc',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  project: {
+    basePath: '/studio',
+  },
 })
